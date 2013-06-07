95fc5abe3c63df00ea8efb7e82559d3c9a5cb779
Adam Saleh <asaleh@redhat.com>
2013-06-07 10:25:56 +0200

    trying to fix compilation

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main tcms-upload.core
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [necessary-evil "2.0.0"]
+                 [slingshot "0.10.3"]
                  [org.clojure/tools.cli "0.2.2"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/data.zip "0.1.1"]])
+                 [org.clojure/data.zip "0.1.1"]]
+  :aot :all
+  :main tcms-upload.core
+  )
