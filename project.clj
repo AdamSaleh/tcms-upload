b96845dfed62feb4177db889007bc9e380316205
Adam Saleh <asaleh@redhat.com>
2013-07-08 09:42:57 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tcms-upload "0.1.7"
+(defproject tcms-upload "0.1.8"
   :description "A simple command-line app for uploading test-ng xml results to tcms test runs."
   :url "https://github.com/AdamSaleh/tcms-upload"
   :license {:name "Eclipse Public License"
